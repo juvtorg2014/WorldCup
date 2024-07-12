fe05065a0e92b2396feb948fff75b0d426c4dce7
juvtorg2014 <juvtorg2014@YANDEX.RU>
2024-07-12 16:08:04 +0300

    Новый конкурс 2024-2025 Global - 20240712

--- wordcap.py
+++ wordcap.py
@@ -15,7 +15,7 @@ headers = {'User-Agent': 'Mozilla/5.0'}
 quarters = {1: [1, 2, 3], 2: [4, 5, 6], 3: [7, 8, 9], 4: [10, 11, 12]}
 
 NAME_WORLD = '2024 World Cup Championship of Futures Trading®'
-NAME_GLOBAL = 'The Global Cup Trading Championship Futures™️ 2023-2024'
+NAME_GLOBAL = 'The Global Cup Trading Championships™️ 2024-2025'
 PAGE = "https://www.worldcupchampionships.com//world-cup-trading-championship-standings"
 
 pack_requests = 'requests'
@@ -108,6 +108,7 @@ def get_page(html, dir_name):
 	"""Получение данных со страницы"""
 	soup = BeautifulSoup(html, 'html.parser')
 	today = []
+	global_list = []
 	# Работа с датами
 	date = soup.find('span', id='tablepress-2024-futures-wcc-description').text
 	year_now = date.split(',')[2].strip()
@@ -151,27 +152,28 @@ def get_page(html, dir_name):
 		data_for_q = find_global(for_st_quarter)
 	
 	# Работа с данными  Global Cup
-	for_g = soup.find('div', class_='has_eae_slider elementor-column elementor-col-33 '
-					'elementor-inner-column elementor-element elementor-element-2ffa355')
-	
-	for_table = for_g.find('table', class_="tablepress tablepress-id-global-cup-forex-23-24 tablepress-responsive")
-	
-	fut = soup.find('div', class_='has_eae_slider elementor-column elementor-col-33 '
-			'elementor-inner-column elementor-element elementor-element-e7c9cd4')
-	
-	fut_table = fut.find('table', class_="tablepress tablepress-id-global-cup-futures-23-24 tablepress-responsive")
-	
-	for_table_global = for_table.find('tbody', class_='row-hover').find_all('tr')
-	data_for_global = find_global(for_table_global)
-	
-	fut_table_global = fut_table.find('tbody', class_='row-hover').find_all('tr')
-	data_fut_global = find_global(fut_table_global)
-	
-	write_futures(data_fut_2024, data_fut_q, data_fut_global, date_string, dir_name, name_qr)
-	write_forex(data_for_2024, data_for_q, data_for_global, date_string, dir_name, name_qr)
+	gl_date = soup.find('span', id ='tablepress-global-cup-futures-forex-24-25-description').text
+	for_table = soup.find('div', class_='elementor-element elementor-element-df368fe elementor-widget elementor-widget-text-editor')
+	global_list = for_table.find('tbody').find_all('tr')
+	data_global = get_global_data(global_list)
+
+	# Запись всех данных в файлы
+	write_futures(data_fut_2024, data_fut_q, data_global, date_string, dir_name, name_qr, gl_date)
+	write_forex(data_for_2024, data_for_q, data_global, date_string, dir_name, name_qr, gl_date)
 
 
-def write_futures(data_fut, data_1q, data_gl, date, dir_d, qr):
+def get_global_data(global_list)->list:
+	"""Получение мест участников The Global Cup Trading Championships™️ 2024-2025"""
+	new_list = []
+	for item in global_list:
+		small_list = []
+		for it in item.find_all('td'):
+			small_list.append(it.text)
+		new_list.append(small_list)
+	return new_list
+
+
+def write_futures(data_fut, data_1q, data_gl, date, dir_d, qr, gl_date):
 	""" Запись данных фьючерса в общий файл"""
 	with open(dir_d + '\\' + 'Fut_' + date + '.csv', 'w', encoding='utf-8', newline='') as f:
 		print("*********FUTURES_WORLD*********")
@@ -190,13 +192,14 @@ def write_futures(data_fut, data_1q, data_gl, date, dir_d, qr):
 		
 		print("*********FUTURES_GLOBAL**********")
 		f.writelines(NAME_GLOBAL + '\n')
+		f.writelines(gl_date + '\n')
 		for item in data_gl:
 			csv.writer(f, delimiter=';').writerow(item)
 			print(item)
 		f.writelines('\n')
 
 
-def write_forex(data_for, data_1q, data_global, date, dird, qr):
+def write_forex(data_for, data_1q, data_global, date, dird, qr, gl_date):
 	""" Запись данных форекса в общий файл"""
 	with open(dird + '\\' + 'For_' + date + '.csv', 'w', encoding='utf-8', newline='') as f:
 		print("**********FOREX_WORLD**********")
@@ -215,6 +218,7 @@ def write_forex(data_for, data_1q, data_global, date, dird, qr):
 		
 		print("*********FOREX_GLOBAL**********")
 		f.writelines(NAME_GLOBAL + '\n')
+		f.writelines(gl_date + '\n')
 		for item in data_global:
 			csv.writer(f, delimiter=';').writerow(item)
 			print(item)
